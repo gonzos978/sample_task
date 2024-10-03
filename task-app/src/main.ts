4967ecf34882f2e0de67443a77f029c74e3348cd
adnan.sehic <adnan.sehic@bragg.group>
2024-10-03 15:22:31 +0200

    add initial Pixi app

--- task-app/src/main.ts
+++ task-app/src/main.ts
@@ -1,24 +1,15 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { Application } from "pixi.js";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+(async () => {
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  let gameContainer = document.getElementById("app") as HTMLElement;
+    const app = new Application();
+    await app.init({
+        width: 800,
+        height: 600,
+        backgroundColor: 0x87CEEB
+    });
+
+    gameContainer.appendChild(app.canvas);
+
+})();
